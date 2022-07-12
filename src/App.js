0462f15c27d4ded1c96970793f8607cda9f695a9
uidamaljith <mailtoamaljith@gmail.com>
2022-07-12 20:06:08 +0530

    'Resolved Signin page conflict'

--- src/App.js
+++ src/App.js
@@ -4,39 +4,39 @@ import MainNav from "./components/Aside/MainNav";
 import Dashboard from './components/Dashboard/Dashboard';
 
 import Emergency from './components/Emergency/Emergency';
-import SignInSide from './components/Login/Login'
+import SignInSide from './components/Login/SignIn';
 
-//import Login from './components/login/login';
+// import Login from './components/Login/login';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
-  let isLogin = (localStorage.token && localStorage.token !== '') ? true : false;
+  // let isLogin = (localStorage.token && localStorage.token !== '') ? true : false;
   return (
 
-    <div className="App">
+    // <div className="App">
       
       
-      {/* <MainNav /> */}
-      {/* <Dashboard /> */}
-      {/* <Emergency /> */}
+    //   {/* <MainNav /> */}
+    //   {/* <Dashboard /> */}
+      // {/* <Emergency /> */}
       <SignInSide />
       
 
-    </div>
+    // </div>
 
-    <Router>
-      <div className="wrapper">
-        {isLogin ? (<MainNav />) : ('')}
-        <Routes>
-          <React.Fragment>
-            <Route path="/" exact element={<Dashboard />} />
-            <Route path="/dashboard" exact element={<Dashboard />} />
-            <Route path="/login" exact element={<Login />} />
-          </React.Fragment>
-        </Routes>
-      </div>
-    </Router>
+    // <Router>
+    //   <div className="wrapper">
+    //     {isLogin ? (<MainNav />) : ('')}
+    //     <Routes>
+    //       <React.Fragment>
+    //         <Route path="/" exact element={<Dashboard />} />
+    //         <Route path="/dashboard" exact element={<Dashboard />} />
+    //         <Route path="/login" exact element={<Login />} />
+    //       </React.Fragment>
+    //     </Routes>
+    //   </div>
+    // </Router>
 
   );
 }
